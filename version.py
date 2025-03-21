4e0bb3de9bd991f22cd101d5e2cb1f3b56f762a1
21lakshh <lakshyapaliwalodd@gmail.com>
2025-03-22 03:13:18 +0530

    Modified all files

--- version.py
+++ version.py
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 import sklearn
 import numpy 
 import pandas 
 import folium 
+import PIL
 
 print(sklearn.__version__)
 print(numpy.__version__)
 print(folium.__version__)
 print(pandas.__version__)
-=======
-import sklearn
-import numpy 
-import pandas 
-import folium 
-
-print(sklearn.__version__)
-print(numpy.__version__)
-print(folium.__version__)
-print(pandas.__version__)
->>>>>>> c1535ecb802c3a804120b9d4fc2eed0c72d543f3
+print(PIL.__version__)
